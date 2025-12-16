33fe4d7f1cf888bbe9070c4b530abe84fdb21e9d
yilin <yilinzh54@gmail.com>
2025-12-16 12:58:23 +0800

    added frontend

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,12 +4,22 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"context"
 
 	"github.com/CVWO/sample-go-app/internal/router"
+	"github.com/jackc/pgx/v5"
 )
 
 func main() {
 	r := router.Setup()
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+	
 	fmt.Print("Listening on port 8000 at http://localhost:8000!")
 
 	log.Fatalln(http.ListenAndServe(":8000", r))
