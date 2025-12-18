fd9ca7adf2243004b7036ffad67e70529c980856
yilin <yilinzh54@gmail.com>
2025-12-18 20:34:33 +0800

    refactor routes file to abstract out repeated code

---
+++ internal/handlers/handle.go
@@ -0,0 +1,42 @@
+//modularise handlers code
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/pkg/errors"
+	"github.com/yiilinzhang/cvwo_assignment/internal/api"
+	"github.com/yiilinzhang/cvwo_assignment/internal/dataaccess"
+)
+
+const (
+	ListUsers = "users.HandleList"
+
+	SuccessfulListUsersMessage = "Successfully listed users"
+	ErrRetrieveDatabase        = "Failed to retrieve database in %s"
+	ErrRetrieveUsers           = "Failed to retrieve users in %s"
+	ErrEncodeView              = "Failed to retrieve users in %s"
+)
+
+func HandleList(conn *pgxpool.Pool, w http.ResponseWriter, r *http.Request) (*api.Response, error) {
+	userList, err := dataaccess.ListUser(conn)
+	if err != nil {
+		return nil, errors.Wrap(err, fmt.Sprintf(ErrRetrieveUsers, ListUsers))
+	}
+
+	data, err := json.Marshal(userList)
+	if err != nil {
+		return nil, errors.Wrap(err, fmt.Sprintf(ErrEncodeView, ListUsers))
+	}
+
+	return &api.Response{
+		Payload: api.Payload{
+			Data: data,
+		},
+		Messages: []string{SuccessfulListUsersMessage},
+	}, nil
+}
+
--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -6,42 +6,31 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/yiilinzhang/cvwo_assignment/internal/api"
+	"github.com/yiilinzhang/cvwo_assignment/internal/handlers/posts"
 	"github.com/yiilinzhang/cvwo_assignment/internal/handlers/topics"
 	"github.com/yiilinzhang/cvwo_assignment/internal/handlers/users"
-	"github.com/yiilinzhang/cvwo_assignment/internal/handlers/posts"
 )
+
+type ListHandler func(conn *pgxpool.Pool, w http.ResponseWriter, r *http.Request) (*api.Response, error)
 
 func GetRoutes(conn *pgxpool.Pool) func(r chi.Router) {
 	//TODO see if there is a way to modularise this code
 	return func(r chi.Router) {
-		r.Get("/users", func(w http.ResponseWriter, req *http.Request) {
-			response, err := users.HandleList(conn, w, req)
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return 
-			}
-
-			w.Header().Set("Content-Type", "application/json")
-			json.NewEncoder(w).Encode(response)
-		})
-		r.Get("/topics", func(w http.ResponseWriter, req *http.Request) {
-			response, err := topics.HandleList(conn, w, req)
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return 
-			}
-
-			w.Header().Set("Content-Type", "application/json")
-			json.NewEncoder(w).Encode(response)
-		})
-		r.Get("/posts", func(w http.ResponseWriter, req *http.Request) {
-			response, err := posts.HandleList(conn, w, req)
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return 
-			}
-			w.Header().Set("Content-Type", "application/json")
-			json.NewEncoder(w).Encode(response)
-		})
+		r.Get("/users", Routing(conn, users.HandleList))
+		r.Get("/topics", Routing(conn, topics.HandleList))
+		r.Get("/posts", Routing(conn, posts.HandleList))
 	}
+}
+
+func Routing(conn *pgxpool.Pool, HandleList ListHandler) http.HandlerFunc {
+		return func(w http.ResponseWriter, req *http.Request) {
+			response, err := HandleList(conn, w, req)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return 
+			}
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(response)
+		}
 }
