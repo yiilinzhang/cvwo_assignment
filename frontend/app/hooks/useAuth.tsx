787b36f43eabf48d073c54e19519ac2c6c9014dc
Zhang Yilin <yilinzh54@gmail.com>
2026-01-18 19:11:33 +0800

    added all use query types

--- frontend/app/hooks/useAuth.tsx
+++ frontend/app/hooks/useAuth.tsx
@@ -1,20 +1,25 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
+type User = {
+  user_id: number;
+  name: string;
+};
+type UserResponse = { payload?: { data?: User } };
 //Custom hook to check if user is authenticated + username
 export function useAuth() {
-  const { isLoading, data, isError } = useQuery({
+  const { isLoading, data, isError } = useQuery<UserResponse>({
     queryKey: ["me"],
     queryFn: async () => {
       const url = "http://localhost:8000/me";
       const response = await axios.get(url, { withCredentials: true });
       return await response.data;
     },
-    retry: false
+    retry: false,
   });
 
   return {
     user: isError ? null : data,
-    isLoading
-  }
+    isLoading,
+  };
 }
